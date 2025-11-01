0224a90904294026048fec62b56cd65555628719
emnuelht <wellingtonemanuel510@gmail.com>
2025-11-01 15:28:15 -0300

    Adicionei a lib do fontawesome para os icones

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
