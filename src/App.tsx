09d6565c271d944d203e355abcda098dc942cb70
emnuelht <wellingtonemanuel510@gmail.com>
2025-11-02 14:18:27 -0300

    Atualizei uma formação do topico de Formação, tanto no site como no pdf, agora está correto, e adicionei a area de projetos que eu criei e ajustei para ficar responsivo tambem no css

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,161 @@ function App() {
                     <div className="list--item">
                         <p className="item--title">Engenharia de Software</p>
                         <p className="item--subtitle">Estácio</p>
-                        <p className="item--date">2020 - 2023</p>
+                        <p className="item--date">2021 - 2025</p>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
+    function Projects() {
+        return (
+            <section className='main--projects part'>
+                <IconTitle title="Projetos" icon="folder-open" />
+
+                <div className="projects--list">
+                    <div className="list--item">
+                        <i className="fa-solid fa-lock item--icon"></i>
+                        <p className="item--title">Formulário de Login Responsivo</p>
+                        <p className="item--description">
+                            Um dos meus primeiros projetos para testar minhas habilidades com design e responsividade. Nada muito complexo, mas foi uma boa forma de praticar CSS, gradientes e media queries. Na época estava aprendendo a fazer layouts que funcionassem bem em diferentes telas.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/form-login">Acessar</a>
+                        </div>
+                        <div className="item--link">
+                            <i className="fa-solid fa-link"></i>
+                            <span className='link--text'>Link do Site:</span>
+                            <a className='link--url' href="https://emnuelht.github.io/form-login/">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-mobile-alt item--icon"></i>
+                        <p className="item--title">ServicesHere - App React Native</p>
+                        <p className="item--description">
+                            Este foi um projeto que fiz durante a faculdade para uma atividade. Eu mesmo escolhi o tema, observei uma necessidade na minha cidade e criei um aplicativo de serviços. A ideia era conectar pessoas que precisam de serviços com profissionais disponíveis na região. O app tinha cadastro de usuários, publicação de serviços, área para profissionais, sistema de busca e muito mais. Mesmo sendo um conceito comum hoje. Infelizmente não funciona mais porque o servidor expirou e acabei não dando continuidade, mas o projeto me rendeu nota máxima e muito aprendizado em React Native, Expo, APIs e backend.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/ServicesHere">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-code item--icon"></i>
+                        <p className="item--title">Gerador de Entities Java</p>
+                        <p className="item--description">
+                            Este foi um projeto experimental onde tentei criar um sistema que identificasse mudanças em estruturas JSON e gerasse automaticamente arquivos Java (entities) para tabelas de banco de dados. Na época eu não sabia que já existiam ferramentas que faziam isso, mas foi uma experiência valiosa! O sistema comparava JSONs, identificava diferenças e gerava tanto as classes Entity quanto scripts SQL de migração. Aprendi muito sobre manipulação de JSON, geração de código dinâmico e estruturas de dados. Mesmo não tendo finalizado completamente, me deu uma base sólida que usei em outros projetos.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/java-api-rest">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-folder-open item--icon"></i>
+                        <p className="item--title">Site Estação Inova - O Projeto que Mudou Minha Vida</p>
+                        <p className="item--description">
+                            Este é um dos projetos mais especiais da minha trajetória, porque foi através dele que consegui minha primeira oportunidade profissional. Em 2020/2021, quando ainda estava aprendendo programação de forma autodidata através do YouTube e cursos gratuitos (muito obrigado, Gustavo Guanabara!), apareceu uma oportunidade única: um curso gratuito de programação na minha cidade chamado Estação Inova.
+                            <br />
+                            Como eu via o curso como algo nunca visto, pois era algo grande e gratuito, eu nem imaginava que seria selecionado. Mas consegui! Durante 6 meses, tive acesso à plataforma Alura e pude estudar JavaScript, design, e muito mais. Vi muitos colegas desistirem no meio do caminho, mas eu me mantive firme porque sabia que era uma oportunidade única.
+                            <br />
+                            No final do curso, o desafio foi criar um site para o próprio Estação Inova. Mesmo sem saber por onde começar, fui pesquisando, testando e montando. Fui o único aluno a desenvolver completamente o código do site (com ajuda apenas nos textos, porque realmente não sou muito bom escrevendo kkk). O resultado foi um site completo com informações sobre o projeto, galeria de cursos, área de projetos dos alunos.
+                            <br />
+                            Quando apresentei o site no final do curso, acabei ganhando uma bolsa de trabalho - onde trabalho até hoje! O link do projeto está logo abaixo, fique a vontade para ver meu projeto.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/estacao-inova">Acessar</a>
+                        </div>
+                        <div className="item--link">
+                            <i className="fa-solid fa-link"></i>
+                            <span className='link--text'>Link do Site:</span>
+                            <a className='link--url' href="https://emnuelht.github.io/estacao-inova/">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-code item--icon"></i>
+                        <p className="item--title">CompareJson - Minha Primeira Biblioteca Java</p>
+                        <p className="item--description">
+                            Este projeto foi especial porque foi através dele que aprendi a criar e publicar uma biblioteca Java própria! A ideia surgiu da necessidade de comparar mudanças estruturais entre arquivos JSON que representavam tabelas de banco de dados.
+                            <br />
+                            O CompareJson identifica automaticamente três tipos de alterações: adições (✅), modificações (🔄) e remoções (❌) em estruturas JSON. Ele funciona com um formato específico onde tabelas usam `__tbXX` e colunas usam `__cnXX` como identificadores únicos. O resultado é um Map com chaves descritivas que mostram exatamente onde cada mudança aconteceu.
+                            <br />
+                            Foi bem legal implementar a lógica de comparação recursiva e entender como publicar no JitPack, mas o resultado foi uma ferramenta que outras pessoas podem usar adicionando apenas uma linha no Gradle! Aprendi muito sobre Jackson para parsing de JSON, recursão e como estruturar uma biblioteca profissional.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/compareJson">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-code item--icon"></i>
+                        <p className="item--title">JsonToSQL - Expandindo a Ideia</p>
+                        <p className="item--description">
+                            Depois de criar o CompareJson, quis complementar o projeto criando algo que gerasse tabelas SQL dinamicamente a partir de JSON. Reutilizei o mesmo formato de estrutura do CompareJson (tabelas com `__tbXX` e colunas com `__cnXX`) para criar uma ferramenta que converte JSON em comandos CREATE TABLE.
+                            <br />
+                            O usuário fornece um JSON seguindo o padrão e o programa retorna SQL pronto para execução. Foi interessante trabalhar com geração dinâmica de código e pensar em como mapear tipos JSON para tipos SQL de forma inteligente.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/json-to-sql">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-solid fa-graduation-cap item--icon"></i>
+                        <p className="item--title">Site de Aula - Minha Experiência Como Professor</p>
+                        <p className="item--description">
+                            Uma das experiências marcantes da minha jornada foi quando tive a oportunidade de ensinar programação para crianças de 11 a 14 anos. Para tornar o aprendizado mais interessante e interativo, criei um site completo que funcionava como uma apresentação e material didático.
+                            <br />
+                            O site cobriu os fundamentos do desenvolvimento web: HTML5 (estrutura e tags), CSS3 (estilos e seletores) e JavaScript (interatividade e DOM). Não usei frameworks propositalmente para que os alunos entendessem a base pura da programação web. Inclui exemplos práticos como um sistema dia/noite do Minecraft que mudava com comandos JavaScript, quiz interativo com validação automática.
+                            <br />
+                            Os alunos conseguiam navegar pelo conteúdo, praticar os exercícios e ver o resultado imediato do código.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/aula-programacao-web">Acessar</a>
+                        </div>
+                        <div className="item--link">
+                            <i className="fa-solid fa-link"></i>
+                            <span className='link--text'>Link do Site:</span>
+                            <a className='link--url' href="https://emnuelht.github.io/aula-programacao-web/">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-brands fa-laravel item--icon"></i>
+                        <p className="item--title">Sistema de Gestão Laravel</p>
+                        <p className="item--description">
+                            Este foi meu primeiro contato com Laravel e PHP, e posso dizer que foi uma experiência muito legal. Criei um sistema completo de gerenciamento hierárquico (Grupos → Bandeiras → Unidades → Colaboradores) que me permitiu explorar profundamente o ecosistema Laravel.
+                            <br />
+                            O sistema possui autenticação segura, CRUD completo com componentes Livewire para interatividade em tempo real, sistema de auditoria que registra todas as ações dos usuários (criação, edição, exclusão), filtragem avançada de dados e exportação para Excel. Vi como o padrão MVC torna o código organizado e escalável.
+                            <br />
+                            Aprendi muito sobre migrations, middleware, validações, blade templates e principalmente sobre Livewire - que me impressionou por permitir criar interfaces dinâmicas usando apenas PHP. O README do projeto é bem detalhado, com prints de todas as telas e explicações do fluxo completo do sistema.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/sistema-gestao">Acessar</a>
+                        </div>
+                    </div>
+                    <div className="list--item">
+                        <i className="fa-brands fa-react item--icon"></i>
+                        <p className="item--title">Portfólio React TypeScript</p>
+                        <p className="item--description">
+                            E por fim, este próprio portfólio que você está vendo! Foi criado com React TypeScript e Vite, focando em design responsivo e performance. Contém todas as informações dos meus projetos, habilidades e trajetória profissional.
+                        </p>
+                        <div className="item--link">
+                            <i className="fa-brands fa-github"></i>
+                            <span className='link--text'>Link do Projeto:</span>
+                            <a className='link--url' href="https://github.com/emnuelht/portifolio">Acessar</a>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -353,6 +507,7 @@ function App() {
             <SoftSkills />
             <HabilitatesProfessional />
             <Formation />
+            <Projects />
             <Languages />
             <PersonalHighlights />
             <Footer />
